b0a87c376c4966f56a9fe8c6f54a469024d4eb0e
Ryan Ray <ryan@trainwrecked.net>
2020-08-31 18:44:12 +1000

    Refactored tests

--- cli_test.go
+++ cli_test.go
@@ -1,13 +1,9 @@
 package cli2
 
 import (
+	"errors"
 	"flag"
 	"testing"
-)
-
-var (
-	name        = "TestCmd"
-	description = "TestDescription"
 )
 
 type TestCmd struct {
@@ -15,8 +11,8 @@ type TestCmd struct {
 	args             []string
 }
 
-func (tc TestCmd) Name() string              { return name }
-func (tc TestCmd) Description() string       { return description }
+func (tc TestCmd) Name() string              { return "TestCmd" }
+func (tc TestCmd) Description() string       { return "TestDescription" }
 func (tc TestCmd) Register(fs *flag.FlagSet) {}
 func (tc *TestCmd) Execute(args []string) error {
 	tc.executionSuccess = true
@@ -24,31 +20,67 @@ func (tc *TestCmd) Execute(args []string) error {
 	return nil
 }
 
+var errCmdReturn error = errors.New("ErrCmdRet")
+
+type ErrorCmd struct{}
+
+func (ec ErrorCmd) Name() string              { return "ErrorCmd" }
+func (ec ErrorCmd) Description() string       { return "ErrorDescription" }
+func (ec ErrorCmd) Register(fs *flag.FlagSet) {}
+func (ec ErrorCmd) Execute(args []string) error {
+	return errCmdReturn
+}
+
 func TestNewApp(t *testing.T) {
 	tc := &TestCmd{false, []string{}}
 	app := NewApp(tc)
 
-	got := app.Command.Name()
-	want := "TestCmd"
-	if got != want {
-		t.Errorf("Got %s, want %s", got, want)
+	tests := []struct {
+		name string
+		got  string
+		want string
+	}{
+		{name: "CmdName", got: app.Command.Name(), want: "TestCmd"},
+		{name: "CmdDescription", got: app.Command.Description(), want: "TestDescription"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.got != tt.want {
+				t.Errorf("Got %s, want %s", tt.got, tt.want)
+			}
+		})
 	}
 }
 
 func TestExecute(t *testing.T) {
-	tc := &TestCmd{false, []string{}}
-	app := NewApp(tc)
-
-	args := []string{"one", "two", "three"}
-	app.Command.Execute(args)
-
-	if !tc.executionSuccess {
-		t.Errorf("Expected true, got false")
+	tests := []struct {
+		name string
+		cmd  *TestCmd
+		args []string
+		err  error
+	}{
+		{name: "EmptyArgs", cmd: &TestCmd{}, args: []string{}},
+		{name: "SingleArg", cmd: &TestCmd{}, args: []string{"arg1"}},
+		{name: "MultipleArgs", cmd: &TestCmd{}, args: []string{"arg1", "arg2", "arg3"}},
+		{name: "DuplicateArgs", cmd: &TestCmd{}, args: []string{"arg", "arg", "arg"}},
+		{name: "ArgAfterFlag", cmd: &TestCmd{}, args: []string{"-f", "arg"}},
+		{name: "OnlyFlags", cmd: &TestCmd{}, args: []string{"-a", "-b"}},
 	}
 
-	for i, v := range tc.args {
-		if v != args[i] {
-			t.Errorf("Got %s, want %s", v, args[i])
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			app := NewApp(tt.cmd)
+			app.Command.Execute(tt.args)
+			if !tt.cmd.executionSuccess {
+				t.Errorf("Command did not successfully execute")
+			}
+
+			for i, arg := range tt.args {
+				if arg != tt.args[i] {
+					t.Errorf("Got %s, want %s", arg, tt.args[i])
+				}
+			}
+		})
 	}
 }
